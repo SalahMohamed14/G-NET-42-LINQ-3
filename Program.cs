942a080575aba9eca797134e87f3f702286a2eb8
Salah Mohamed <ssalah123mohamed@gmail.com>
2026-04-18 16:32:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace G_NET_42_LINQ_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q13
+            //int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+            //int[] setB = { 3, 6, 9, 12, 15, 13 };
+
+            //var res = setA.Except(setB);
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //} 
+            #endregion
+        }
+    }
+}
