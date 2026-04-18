94751669b634ec729fc204a013b6b52b4021d4b8
Salah Mohamed <ssalah123mohamed@gmail.com>
2026-04-18 16:51:11 +0200

    Question20

--- Program.cs
+++ Program.cs
@@ -84,6 +84,19 @@ namespace G_NET_42_LINQ_3
             //} 
             #endregion
 
+            #region Q20
+            //var result20 = ProductList
+            //    .Take(3)
+            //    .Select(p => p.ProductName)
+            //    .Concat(customers.Take(3).Select(c => c.CompanyName));
+
+            //foreach (var item in result20)
+            //{
+            //    Console.WriteLine(item);
+            //} 
+            #endregion
+
+
         }
     }
 }
